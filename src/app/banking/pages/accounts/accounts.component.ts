8f222a0efed2acea9077adc630926c4d2f1d3f5b
Zuleidy Yaruro <49843884+zuleidyyaruro@users.noreply.github.com>
2024-03-31 14:34:16 -0500

    feat: accounts ok

--- src/app/banking/pages/accounts/accounts.component.ts
+++ src/app/banking/pages/accounts/accounts.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { ProfileBehaviorService } from '../../../shared/services/profile-behavior.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-accounts',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './accounts.component.html',
-  styleUrl: './accounts.component.css'
+  styleUrl: './accounts.component.css',
 })
 export class AccountsComponent {
+  public accounts: string[] = [];
 
+  constructor(private profileBehaviorService: ProfileBehaviorService) {}
+
+  ngOnInit(): void {
+    this.getProfile();
+  }
+
+  getProfile(): void {
+    this.profileBehaviorService.getProfile().subscribe((res) => {
+      this.accounts = res.accounts;
+    });
+  }
 }
