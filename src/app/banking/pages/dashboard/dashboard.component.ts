f15daa5aeb44192fe3fb1b63d0eba53ac640b436
Zuleidy Yaruro <49843884+zuleidyyaruro@users.noreply.github.com>
2024-03-31 13:36:01 -0500

    transactions in dashboard

--- src/app/banking/pages/dashboard/dashboard.component.ts
+++ src/app/banking/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Profile } from '../../interfaces/profile.interface';
 import { ProfileBehaviorService } from '../../../shared/services/profile-behavior.service';
+import { TransactionsService } from '../../services/transactions.service';
+import { Transaction } from '../../interfaces/transaction.interface';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,15 +13,45 @@ import { ProfileBehaviorService } from '../../../shared/services/profile-behavio
 })
 export class DashboardComponent {
   public dataProfile: Profile = {} as Profile;
-  constructor(private profileBehaviorService: ProfileBehaviorService) {}
+  public transactionData: Transaction[] = [];
+  constructor(
+    private profileBehaviorService: ProfileBehaviorService,
+    private transactionsService: TransactionsService
+  ) {}
 
   ngOnInit() {
     this.getProfile();
+    this.getTransactions();
   }
 
   getProfile(): void {
     this.profileBehaviorService.getProfile().subscribe((res) => {
       this.dataProfile = res;
     });
+  }
+
+  getTransactions(): void {
+    this.transactionsService.getTransactions().subscribe({
+      next: (data: Transaction[]) => {
+        this.transactionData = data.slice(0, 10);
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
+      },
+    });
+  }
+
+  get transactionAmount(): number {
+    return this.transactionData.reduce(
+      (acumulador, transaccion) => acumulador + transaccion.amount,
+      0
+    );
+  }
+
+  get transactionBalance(): number {
+    return this.transactionData.reduce(
+      (acumulador, transaccion) => acumulador + transaccion.balance,
+      0
+    );
   }
 }
