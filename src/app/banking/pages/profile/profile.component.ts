3e0d4140c2a28af324a84300f81da1863d98de81
Zuleidy Yaruro <49843884+zuleidyyaruro@users.noreply.github.com>
2024-03-30 23:24:57 -0500

    feat: profile ok

--- src/app/banking/pages/profile/profile.component.ts
+++ src/app/banking/pages/profile/profile.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { Profile } from '../../interfaces/profile.interface';
+import { ProfileBehaviorService } from '../../../shared/services/profile-behavior.service';
+import { NgbRating } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [NgbRating],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
 export class ProfileComponent {
+  public dataProfile: Profile = {} as Profile;
+  public rating = 8;
+  constructor(private profileBehaviorService: ProfileBehaviorService) {}
 
+  ngOnInit() {
+    this.getProfile();
+  }
+
+  getProfile(): void {
+    this.profileBehaviorService.getProfile().subscribe((res) => {
+      this.dataProfile = res;
+    });
+  }
 }
